ae55e752e8d719622df2572b5c72e414d37a1b8e
codingmatemoon <abc9529@naver.com>
2023-04-04 08:20:23 +0900

    파생 변수를 질의 함수로 바꾸기 Replace Derived Variable with Query / [Discount] setDiscount에서 계산을 통해 설정하던 discountedTotal 파생 변수를 calculatedDiscountTotal 함수를 통해 가져오도록 변경 [ProductionPlan] (1) this.production += adjustment+ 로 production값을 구하는 방식을 calculatedProduction 함수로 구하도록 변경. (2) Inline Method(CTLR+ALT+N) : calculatedProduction 계산 함수 호출값 return에서 람다식 return 방식으로 변경

--- src/main/java/moon/codingmate/_06_mutable_data/_21_replace_derived_variable_with_query/Discount.java
+++ src/main/java/moon/codingmate/_06_mutable_data/_21_replace_derived_variable_with_query/Discount.java
@@ -2,7 +2,7 @@ package moon.codingmate._06_mutable_data._21_replace_derived_variable_with_query
 
 public class Discount {
 
-    private double discountedTotal;
+//    private double discountedTotal;
     private double discount;
 
     private double baseTotal;
@@ -11,12 +11,19 @@ public class Discount {
         this.baseTotal = baseTotal;
     }
 
+    // discountedTotal은 setDiscount를 통해 this.baseTotal - this.discount로 계산했을 때만 값을 가져올 수 있습니다.
     public double getDiscountedTotal() {
-        return this.discountedTotal;
+//        assert this.discountedTotal == this.baseTotal - this.discount;
+//        assert this.discountedTotal == calculatedDiscountedTotal();
+        return calculatedDiscountedTotal();
+    }
+
+    private double calculatedDiscountedTotal() {
+        return this.baseTotal - this.discount;
     }
 
     public void setDiscount(double number) {
         this.discount = number;
-        this.discountedTotal = this.baseTotal - this.discount;
+        //this.discountedTotal = this.baseTotal - this.discount;
     }
 }
--- src/main/java/moon/codingmate/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
+++ src/main/java/moon/codingmate/_06_mutable_data/_21_replace_derived_variable_with_query/ProductionPlan.java
@@ -10,10 +10,28 @@ public class ProductionPlan {
 
     public void applyAdjustment(double adjustment) {
         this.adjustments.add(adjustment);
-        this.production += adjustment;
+//        this.production += adjustment;
     }
 
+    /* (1) this.production += adjustment+ 로 production값을 구하는 방식을 calculatedProduction 함수로 구하도록 변경
     public double getProduction() {
+        assert this.production == calculatedProduction();
         return this.production;
+    }
+
+    private double calculatedProduction() {
+        // identity 초기값이 있고 2개의 값을 받아서 합을 계산.
+        //return this.adjustments.stream().reduce((double) 0, (a, b) -> a + b);
+//        return this.adjustments.stream().reduce((double) 0, Double::sum);
+        return this.adjustments.stream().mapToDouble(Double::valueOf).sum();
+    }
+     */
+    // (2) Inline Method(CTLR+ALT+N) : calculatedProduction 계산 함수 호출값 return에서 람다식 return 방식으로 변경
+    public double getProduction() {
+        // identity 초기값이 있고 2개의 값을 받아서 합을 계산.
+        //return this.adjustments.stream().reduce((double) 0, (a, b) -> a + b);
+//        return this.adjustments.stream().reduce((double) 0, Double::sum);
+        // derived variable -> 값을 리턴하는 함수, 조회하는 함수 query로 변환
+        return this.adjustments.stream().mapToDouble(Double::valueOf).sum();
     }
 }
