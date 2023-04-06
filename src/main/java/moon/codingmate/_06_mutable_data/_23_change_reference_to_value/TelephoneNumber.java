f9b0c46f1d635a4524eea1aec75a1adaf5c4be5d
codingmatemoon <abc9529@naver.com>
2023-04-07 08:01:18 +0900

    참조를 값으로 바꾸기 Change Reference to Value / TelephoneNumber : setter 제거, 필드 final로 변경하고 생성할 때 값을 한번 할당, equals, hashCode 구현(record 형식) -> record 형식으로 변경 Person : officeAreaCode, officeNumber setter 대신 new TelephoneNumber로 값 객체 생성

--- src/main/java/moon/codingmate/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
+++ src/main/java/moon/codingmate/_06_mutable_data/_23_change_reference_to_value/TelephoneNumber.java
@@ -1,24 +1,38 @@
 package moon.codingmate._06_mutable_data._23_change_reference_to_value;
 
-public class TelephoneNumber {
+public record TelephoneNumber(String areaCode, String number) {
+    /*
+    private final String areaCode;
 
-    private String areaCode;
+    private final String number;
 
-    private String number;
+    public TelephoneNumber(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
 
     public String areaCode() {
         return areaCode;
-    }
-
-    public void areaCode(String areaCode) {
-        this.areaCode = areaCode;
     }
 
     public String number() {
         return number;
     }
 
-    public void number(String number) {
-        this.number = number;
+    //hashCode : equals와 같이 구현해야 합니다. TelephoneNumber 인스턴스가 collection 안에 들어갈 때 특히 hashset 같은 collection에 들어갈 때 값이 같으면
+    //hashcode도 같아야하고 값이 다르면 hashcode도 달라야하기 때문입니다.
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TelephoneNumber that = (TelephoneNumber) o;
+        return Objects.equals(areaCode, that.areaCode) && Objects.equals(number, that.number);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
+    }
+    */
 }
