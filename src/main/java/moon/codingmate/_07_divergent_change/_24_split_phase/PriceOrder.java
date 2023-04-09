defb255e8b8be03acbd6e10d805d30880a4adfe7
codingmatemoon <abc9529@naver.com>
2023-04-09 12:06:59 +0900

    리팩토링 24. 단계 쪼개기 Split Phase (1)Extract Method : shippingCost 계산 구현 로직 함수로 추출 (2)중간 데이터(immediate data) record 생성 및 signature 변경 (3) Extract Method : PriceData 계산 구현 로직을calculatePriceData 함수로 추출. inline을 통해 라인수 줄이기
    어떤 메서드 function안에서 서로 다른 일을 하는 코드를 각기 다른 모듈로 분리합니다.
    하나의 함수는 한 가지 일만 하도록 하는 것이 변경 및 테스트 코드 작성에 좋습니다.
    메서드가 클 경우 어떤 프로세스, 흐름이 있는데 그 흐름을 구체적으로 나누고 별도의 메서드로 분리하고 그 메서드의 위치가 적절하지 않으면 다른 모듈로 메서드를 옮기는 리팩토링의 전 단계 리팩토링으로 split phase를 활용할 수 있씁니다.
    컴파일러 : 텍스트 읽어오기 -> 실행 가능한 형태로 변경
    더 구체적인 작업으로 나누고 나눈 작업을 별도의 클래스로 옮길 수도 있고 관련 있는 작업들을 서로 묶을 수도 있습니다.
    이러한 단계를 나누는 기준, 단서로 각각의 코드가 사용하는 데이터를 기반으로 생각하면 좋습니다. 사용하는 데이터가 다르다면 그 둘은 다른 프로세스를 타고 있을 수 있습니다. 이러한 것들을 단서로 삼아 여러 단계로 프로세스를 쪼갭니다. 이렇게 쪼갤 때 intermediate data 중간 데이터를 만들어서 쪼갠 메서드들에 전달합니다.

--- src/main/java/moon/codingmate/_07_divergent_change/_24_split_phase/PriceOrder.java
+++ src/main/java/moon/codingmate/_07_divergent_change/_24_split_phase/PriceOrder.java
@@ -3,13 +3,24 @@ package moon.codingmate._07_divergent_change._24_split_phase;
 public class PriceOrder {
 
     public double priceOrder(Product product, int quantity, ShippingMethod shippingMethod) {
+        final PriceData priceData = calculatePriceData(product, quantity);
+        return applyShipping(priceData, shippingMethod);
+    }
+
+    private PriceData calculatePriceData(Product product, int quantity) {
         final double basePrice = product.basePrice() * quantity;
         final double discount = Math.max(quantity - product.discountThreshold(), 0)
-                * product.basePrice() * product.discountRate();
-        final double shippingPerCase = (basePrice > shippingMethod.discountThreshold()) ?
-                shippingMethod.discountedFee() : shippingMethod.feePerCase();
-        final double shippingCost = quantity * shippingPerCase;
-        final double price = basePrice - discount + shippingCost;
+                * product.basePrice() * product.discountRate(); // 분기 2가지
+        // immediate data
+        final PriceData priceData = new PriceData(basePrice, discount, quantity);
+        return priceData;
+    }
+
+    private double applyShipping(PriceData priceData, ShippingMethod shippingMethod) {
+        final double shippingPerCase = (priceData.basePrice() > shippingMethod.discountThreshold()) ?
+                shippingMethod.discountedFee() : shippingMethod.feePerCase(); // 분기 2가지 -> 총 2x2 = 4 case에 대한 테스트 필요
+        final double shippingCost = priceData.quantity() * shippingPerCase;
+        final double price = priceData.basePrice() - priceData.discount() + shippingCost;
         return price;
     }
 }
