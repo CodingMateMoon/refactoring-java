1808c2ffdceba1868560a2c5b2b0d4025bf26523
codingmatemoon <abc9529@naver.com>
2023-04-13 07:44:32 +0900

    리팩토링 28. 함수 인라인 Inline Function moreThanFiveLateDeliveries : 5회 이상 지연 시 rating 2, 아니면 1로 처리하는데 내부 구현을 봐도 충분히 의미를 파악할 수 있습니다.

--- src/main/java/moon/codingmate/_08_shotgun_surgery/_28_inline_function/Rating.java
+++ src/main/java/moon/codingmate/_08_shotgun_surgery/_28_inline_function/Rating.java
@@ -2,11 +2,14 @@ package moon.codingmate._08_shotgun_surgery._28_inline_function;
 
 public class Rating {
 
+    // moreThanFiveLateDeliveries : 5회 이상 지연 시 rating 2, 아니면 1로 처리하는데 내부 구현을 봐도 충분히 의미를 파악할 수 있습니다
     public int rating(Driver driver) {
-        return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+        return driver.getNumberOfLateDeliveries() > 5 ? 2 : 1;
     }
 
+    /*
     private boolean moreThanFiveLateDeliveries(Driver driver) {
         return driver.getNumberOfLateDeliveries() > 5;
     }
+     */
 }
