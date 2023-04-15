9adc474b23d77c5fd54b1887dedb2718b12999ab
codingmatemoon <abc9529@naver.com>
2023-04-15 09:27:08 +0900

    리팩토링 29. 클래스 인라인 Inline Class - Trackinginformation의 필드, 메서드를 복사해온 뒤 해당 클래스의 메서드를 사용하는 Shipment의 메서드를 전부 제거 및 변경

--- src/main/java/moon/codingmate/_08_shotgun_surgery/_29_inline_class/Shipment.java
+++ src/main/java/moon/codingmate/_08_shotgun_surgery/_29_inline_class/Shipment.java
@@ -2,8 +2,18 @@ package moon.codingmate._08_shotgun_surgery._29_inline_class;
 
 public class Shipment {
 
-    private TrackingInformation trackingInformation;
+//    private TrackingInformation trackingInformation;
 
+    private String shippingCompany;
+
+    private String trackingNumber;
+
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
+    }
+
+    /* Trackinginformation의 필드, 메서드를 복사해온 뒤 해당 클래스의 메서드를 사용하는 Shipment의 메서드를 전부 제거 및 변경
     public Shipment(TrackingInformation trackingInformation) {
         this.trackingInformation = trackingInformation;
     }
@@ -15,8 +25,32 @@ public class Shipment {
     public void setTrackingInformation(TrackingInformation trackingInformation) {
         this.trackingInformation = trackingInformation;
     }
+     */
 
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+//        return this.trackingInformation.display();
+        return this.shippingCompany + ": " + this.trackingNumber;
+    }
+
+    /* inline method
+    public String display() {
+        return this.shippingCompany + ": " + this.trackingNumber;
+    }
+     */
+
+    public String getShippingCompany() {
+        return shippingCompany;
+    }
+
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
     }
 }
