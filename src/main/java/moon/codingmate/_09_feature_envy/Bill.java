129564180c25ee4b5b4debd474195bbb1935dd63
codingmatemoon <abc9529@naver.com>
2023-04-16 13:16:41 +0900

    냄새 9. 기능 편애 Feature Envy - Bill 클래스가 electricityUsage, gasUsage 전기 가스 사용 계산에 대한 feature에 욕심을 내고 있습니다. Usage 클래스에서도 계산을 할 수 있기에 Move Function으로 옮깁니다.

--- src/main/java/moon/codingmate/_09_feature_envy/Bill.java
+++ src/main/java/moon/codingmate/_09_feature_envy/Bill.java
@@ -7,9 +7,16 @@ public class Bill {
     private GasUsage gasUsage;
 
     public double calculateBill() {
+
+        return electricityUsage.getElecticityBill() + gasUsage.getGasBill();
+    }
+    /*
+    public double calculateBill() {
+        // Bill 클래스가 electricityUsage, gasUsage 전기 가스 사용 계산에 대한 feature에 욕심을 내고 있습니다. Usage 클래스에서도 계산을 할 수 있기에 Move Function 옮깁니다.
         var electicityBill = electricityUsage.getAmount() * electricityUsage.getPricePerUnit();
         var gasBill = gasUsage.getAmount() * gasUsage.getPricePerUnit();
         return electicityBill + gasBill;
     }
+     */
 
 }
