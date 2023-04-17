ce6c57f7bde6e6cdc05a674c533809727a6cd9fe
codingmatemoon <abc9529@naver.com>
2023-04-17 22:20:47 +0900

    냄새 10. 데이터 뭉치 Data Clumps - Employee, Office class의 areaCode, number 필드 및 관련 메서드를 TelephoneNumber 클래스로 이동 (Extract Class)

--- src/main/java/moon/codingmate/_10_data_clumps/Employee.java
+++ src/main/java/moon/codingmate/_10_data_clumps/Employee.java
@@ -3,19 +3,17 @@ package moon.codingmate._10_data_clumps;
 public class Employee {
 
     private String name;
+    private TelephoneNumber personalPhoneNumber;
 
-    private String personalAreaCode;
-
-    private String personalNumber;
-
-    public Employee(String name, String personalAreaCode, String personalNumber) {
+    public Employee(String name, TelephoneNumber personalPhoneNumber) {
         this.name = name;
-        this.personalAreaCode = personalAreaCode;
-        this.personalNumber = personalNumber;
+        this.personalPhoneNumber = personalPhoneNumber;
     }
 
     public String personalPhoneNumber() {
-        return personalAreaCode + "-" + personalNumber;
+
+//        return personalAreaCode + "-" + personalNumber;
+        return this.personalPhoneNumber.toString();
     }
 
     public String getName() {
@@ -26,6 +24,21 @@ public class Employee {
         this.name = name;
     }
 
+    public TelephoneNumber getPersonalPhoneNumber() {
+        return personalPhoneNumber;
+    }
+
+    /* 데이터 뭉치
+    private String personalAreaCode;
+    private String personalNumber;
+    public Employee(String name, String personalAreaCode, String personalNumber) {
+        this.name = name;
+        this.personalAreaCode = personalAreaCode;
+        this.personalNumber = personalNumber;
+    }
+     */
+
+    /*
     public String getPersonalAreaCode() {
         return personalAreaCode;
     }
@@ -41,4 +54,6 @@ public class Employee {
     public void setPersonalNumber(String personalNumber) {
         this.personalNumber = personalNumber;
     }
+
+     */
 }
--- src/main/java/moon/codingmate/_10_data_clumps/Office.java
+++ src/main/java/moon/codingmate/_10_data_clumps/Office.java
@@ -4,6 +4,29 @@ public class Office {
 
     private String location;
 
+    private TelephoneNumber officePhoneNumber;
+
+    public Office(String location, TelephoneNumber officePhoneNumber) {
+        this.location = location;
+        this.officePhoneNumber = officePhoneNumber;
+    }
+
+    public String officePhoneNumber() {
+//        return officeAreCode + "-" + officeNumber;
+        return this.officePhoneNumber.toString();
+    }
+
+    public String getOfficeAreCode() {
+//        return officeAreCode;
+        return this.officePhoneNumber.getPersonalAreaCode();
+    }
+
+    public void setOfficeAreCode(String officeAreCode) {
+//        this.officeAreCode = officeAreCode;
+        this.officePhoneNumber.setPersonalAreaCode(officeAreCode);
+    }
+
+    /*
     private String officeAreCode;
 
     private String officeNumber;
@@ -13,18 +36,14 @@ public class Office {
         this.officeAreCode = officeAreCode;
         this.officeNumber = officeNumber;
     }
+     */
 
+    /*
     public String officePhoneNumber() {
         return officeAreCode + "-" + officeNumber;
     }
 
-    public String getOfficeAreCode() {
-        return officeAreCode;
-    }
 
-    public void setOfficeAreCode(String officeAreCode) {
-        this.officeAreCode = officeAreCode;
-    }
 
     public String getOfficeNumber() {
         return officeNumber;
@@ -32,5 +51,5 @@ public class Office {
 
     public void setOfficeNumber(String officeNumber) {
         this.officeNumber = officeNumber;
-    }
+    }*/
 }
