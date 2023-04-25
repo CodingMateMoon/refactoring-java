36b4950cb9bc104cbc1174dded1e2fb2d0889554
codingmatemoon <abc9529@naver.com>
2023-04-26 07:46:43 +0900

    죽은 코드 제거하기(Remove Dead Code) : usage를 본 뒤 테스트코드에서만 참조하고 있는 것을 확인하고 불필요한 코드 제거 Settings-Editor-Inlay Hints-Java-Code vision-Show hints for: Usages 체크

--- src/main/java/moon/codingmate/_15_speculative_generality/_35_remove_dead_code/Reservation.java
+++ src/main/java/moon/codingmate/_15_speculative_generality/_35_remove_dead_code/Reservation.java
@@ -2,6 +2,9 @@ package moon.codingmate._15_speculative_generality._35_remove_dead_code;
 
 import java.time.LocalDateTime;
 
+/* 죽은 코드 제거하기(Remove Dead Code) : usage를 본 뒤 테스트코드에서만 참조하고 있는 것을 확인하고 불필요한 코드 제거
+Settings-Editor-Inlay Hints-Java-Code vision-Show hints for: Usages 체크
+ */
 public class Reservation {
 
     private String title;
@@ -17,7 +20,6 @@ public class Reservation {
         this.from = from;
         this.to = to;
     }
-
     public void setAlarmBefore(int minutes) {
         this.alarm = this.from.minusMinutes(minutes);
     }
