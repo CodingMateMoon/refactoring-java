630941c0d0a2ba5f2cf1647720aca1ecb4802bf7
codingmatemoon <abc9529@naver.com>
2023-04-28 08:30:15 +0900

    리팩토링 36. 특이 케이스 추가하기 Introduce Special Case Extract Method : customer.getName().equals(“unknown”) 조건 체크 로직을 메서드로 추출 Move Instance Method: CustomerService 클래스 내에서 Customer 클래스의 필드값을 사용하는 메서드를 Customer 클래스로 이동 Site 클래스 : 이름이 unknown인 경우 Unknown 클래스를 생성해서 받도록 로직 추가. Replace Conditional with Polymorphism : unknown 여부에 따라 다르게 가져오는 Customer 클래스의 getName, getBillingPlan에 대해 null일 경우 Customer 클래스를 상속받은 UnknownCustomer 클래스의 필드를 반환하는 방식으로 변경하여 분기 처리 로직 제거 PaymentHistory 클래스의 getWeeksDelinquentInLastYear에 대해 null일 경우 PaymentHistory 클래스를 상속받은 NullPaymentHistory 클래스의 getWeeksDelinquentInLastYear을 호출하도록 변경하여 분기 처리 로직 제거

--- src/main/java/moon/codingmate/_16_temporary_field/_36_introduce_special_case/CustomerService.java
+++ src/main/java/moon/codingmate/_16_temporary_field/_36_introduce_special_case/CustomerService.java
@@ -3,26 +3,36 @@ package moon.codingmate._16_temporary_field._36_introduce_special_case;
 public class CustomerService {
 
     public String customerName(Site site) {
+        return site.getCustomer().getName();
+        /*
         Customer customer = site.getCustomer();
 
         String customerName;
-        if (customer.getName().equals("unknown")) {
+        if (customer.isUnknown()) {
             customerName = "occupant";
         } else {
             customerName = customer.getName();
         }
 
         return customerName;
+         */
     }
 
     public BillingPlan billingPlan(Site site) {
+        return site.getCustomer().getBillingPlan();
+        /*
         Customer customer = site.getCustomer();
-        return customer.getName().equals("unknown") ? new BasicBillingPlan() : customer.getBillingPlan();
+        return customer.isUnknown() ? new BasicBillingPlan() : customer.getBillingPlan();
+         */
     }
 
+    // 얼마나 연체, 미납 (overdue)
     public int weeksDelinquent(Site site) {
+        return site.getCustomer().getPaymentHistory().getWeeksDelinquentInLastYear();
+        /*
         Customer customer = site.getCustomer();
-        return customer.getName().equals("unknown") ? 0 : customer.getPaymentHistory().getWeeksDelinquentInLastYear();
+        return customer.isUnknown() ? 0 : customer.getPaymentHistory().getWeeksDelinquentInLastYear();
+         */
     }
 
 }
---
+++ src/main/java/moon/codingmate/_16_temporary_field/_36_introduce_special_case/NullPaymentHistory.java
@@ -0,0 +1,8 @@
+package moon.codingmate._16_temporary_field._36_introduce_special_case;
+
+public class NullPaymentHistory extends PaymentHistory{
+
+    public NullPaymentHistory(){
+        super(0);
+    }
+}
