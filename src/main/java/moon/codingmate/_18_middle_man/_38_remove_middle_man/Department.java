dde227c82c8b74adf2c109857ccff9a4e48be0e0
codingmatemoon <abc9529@naver.com>
2023-04-30 12:59:38 +0900

    리팩토링 38. 중재자 제거하기 Remove Middle Man - department에 있는 여러 필드를 사용하는 것이 필요할 경우 캡슐화했던 것을 빼내고 getDepartment().getManager()를 통해 필요한 필드를 가져오도록 변경합니다

--- src/main/java/moon/codingmate/_18_middle_man/_38_remove_middle_man/Department.java
+++ src/main/java/moon/codingmate/_18_middle_man/_38_remove_middle_man/Department.java
@@ -2,6 +2,7 @@ package moon.codingmate._18_middle_man._38_remove_middle_man;
 
 public class Department {
 
+    // name, number, 접근 가능 시간 등 여러 정보가 있다면 모든 정보를 Person을 거쳐서 접근할 것인지에 대해 고민이 필요합니다.
     private Person manager;
 
     public Department(Person manager) {
--- src/main/java/moon/codingmate/_18_middle_man/_38_remove_middle_man/Person.java
+++ src/main/java/moon/codingmate/_18_middle_man/_38_remove_middle_man/Person.java
@@ -11,7 +11,13 @@ public class Person {
         this.department = department;
     }
 
+    /* department에 있는 여러 필드를 사용하는 것이 필요할 경우 캡슐화했던 것을 빼내고 getDepartment().getManager()를 통해 필요한 필드를 가져오도록 변경합니다
     public Person getManager() {
         return this.department.getManager();
+    }
+     */
+
+    public Department getDepartment() {
+        return department;
     }
 }
