a64da951e974c6b6b34ab5756ce7a8c1045cb3fc
codingmatemoon <abc9529@naver.com>
2023-04-30 11:19:38 +0900

    리팩토링 37. 위임 숨기기 Hide Delegate- Person 클래스의 getDepartment().getManager()를 통해 departmen를 거쳐 manage를 가져오는 방식을 숨겨서 getManager()로 가져올 수 있도록 함수 추출 및 이동

--- src/main/java/moon/codingmate/_17_message_chain/_37_hide_delegate/Person.java
+++ src/main/java/moon/codingmate/_17_message_chain/_37_hide_delegate/Person.java
@@ -6,6 +6,13 @@ public class Person {
 
     private Department department;
 
+    // 추후에 Person 클래스를 필드로 두고 getManager를 통해 가져올 경우 함수 이름을 그대로 사용할 수 있습니다
+    private Person manager;
+
+    Person getManager() {
+        return this.manager;
+    }
+
     public Person(String name) {
         this.name = name;
     }
@@ -20,5 +27,9 @@ public class Person {
 
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    Person getManager_backup() {
+        return getDepartment().getManager();
     }
 }
