83af633bed8144c168f3dc4590e7b42228f19bc7
codingmatemoon <abc9529@naver.com>
2023-05-01 12:52:12 +0900

    슈퍼클래스를 위임으로 바꾸기(Replace Superclass with Delegate): Srcoll 클래스에서 CategoryItem을 상속 대신 필드로 두고 파라미터로 입력받은 값들을 통해 CategoryItem 객체를 생성하고 사용합니다

--- src/main/java/moon/codingmate/_18_middle_man/_39_replace_superclass_with_delegate/CategoryItem.java
+++ src/main/java/moon/codingmate/_18_middle_man/_39_replace_superclass_with_delegate/CategoryItem.java
@@ -2,6 +2,7 @@ package moon.codingmate._18_middle_man._39_replace_superclass_with_delegate;
 
 import java.util.List;
 
+// 남성복 여성복 모자 바지 신발
 public class CategoryItem {
 
     private Integer id;
--- src/main/java/moon/codingmate/_18_middle_man/_39_replace_superclass_with_delegate/Scroll.java
+++ src/main/java/moon/codingmate/_18_middle_man/_39_replace_superclass_with_delegate/Scroll.java
@@ -4,13 +4,19 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-public class Scroll extends CategoryItem {
+// 스크롤 주기적 청소. 아이템이지 카테고리는 아닙니다. 더 이상 상속 구조를 유지하기 어렵겠다고 판단할 경우
+//public class Scroll extends CategoryItem {
+public class Scroll  {
 
     private LocalDate dateLastCleaned;
 
+    //상위 타입을 필드로 선언
+    private CategoryItem categoryItem;
+
     public Scroll(Integer id, String title, List<String> tags, LocalDate dateLastCleaned) {
-        super(id, title, tags);
+        //super(id, title, tags);
         this.dateLastCleaned = dateLastCleaned;
+        this.categoryItem = new CategoryItem(id, title, tags);
     }
 
     public long daysSinceLastCleaning(LocalDate targetDate) {
