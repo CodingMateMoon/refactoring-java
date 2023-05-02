15444f844abfcc3d68ebb672e5816877ade4cff0
codingmatemoon <abc9529@naver.com>
2023-05-03 04:53:24 +0900

    냄새 19. 내부자 거래 Insider Trading - CheckIn에서 FastPass 여부를 판단하기 위해  Ticket 내부 정보를 많이 참조하는데 Ticket쪽으로 해당 메서드를 옮김으로써 CheckIn에 대한 결합도를 낮추고 Ticket에 대한 응집도를 높입니다.

--- src/main/java/moon/codingmate/_19_insider_trading/Ticket.java
+++ src/main/java/moon/codingmate/_19_insider_trading/Ticket.java
@@ -20,4 +20,10 @@ public class Ticket {
     public boolean isPrime() {
         return prime;
     }
+
+    // 빨리 처리할 수 있는 FastPass 여부를 판단하기 위해 티켓 내부 정보를 많이 참조합니다. 이 메서드의 위치는 CheckIn이 아닌 Ticket이 적절하다고 판단할 수도 있습니다. CheckIn에서 부적절하게 FastPass 여부를 판단하기 위해 티켓 내부 정보를 참조하며 생긴 결합도에 대해 Ticket으로 옮기면서 CheckIn에 대한 결합도를 제거할 수 있습니다.
+    public boolean isFastPass() {
+        LocalDate earlyBirdDate = LocalDate.of(2022, 1, 1);
+        return isPrime() && getPurchasedDate().isBefore(earlyBirdDate);
+    }
 }
