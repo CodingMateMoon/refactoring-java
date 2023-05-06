c08fdbe9204c07cd14d6e716b9d96656c36181a8
codingmatemoon <abc9529@naver.com>
2023-05-06 10:46:45 +0900

    리팩토링 42. 레코드 캡슐화하기 Encapsulate Record - java record를 활용하여 불변 필드를 가진 데이터 클래스로 변경. public 필드를 사용하는 코드를 private 필드와 게터, 세터를 사용하도록 변경

--- src/main/java/moon/codingmate/_22_data_class/_42_encapsulate_record/Organization.java
+++ src/main/java/moon/codingmate/_22_data_class/_42_encapsulate_record/Organization.java
@@ -1,9 +1,10 @@
 package moon.codingmate._22_data_class._42_encapsulate_record;
 
-public class Organization {
+// capsulation, 불변 필드를 가진 데이터 클래스
+public record Organization(String name, String country) {
 
-    public String name;
+//    public String name;
 
-    public String country;
+//    public String country;
 
 }
---
+++ src/test/java/moon/codingmate/_22_data_class/_42_encapsulate_record/OrganizationTest.java
@@ -0,0 +1,16 @@
+package moon.codingmate._22_data_class._42_encapsulate_record;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrganizationTest {
+
+    @Test
+    void name() {
+        Organization organization = new Organization("codingmatemoon", "USA");
+
+//        organization.name = "codingmatemoon";
+    }
+
+}
