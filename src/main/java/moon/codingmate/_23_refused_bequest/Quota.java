a415b82e44391f0bb4a8d38739caa112d6481660
codingmatemoon <abc9529@naver.com>
2023-05-07 13:45:45 +0900

    냄새 23. 상속 포기 Refused Bequest(유산) : Quato 할당량은 Employee, Engineer 보다 Saleman에 특화된 메서드이므로 Push Method/Field down을 통해 하위클래스로 내려줍니다.

--- src/main/java/moon/codingmate/_23_refused_bequest/Quota.java
+++ src/main/java/moon/codingmate/_23_refused_bequest/Quota.java
@@ -1,4 +1,5 @@
 package moon.codingmate._23_refused_bequest;
 
+// 할당량
 public class Quota {
 }
--- src/main/java/moon/codingmate/_23_refused_bequest/Salesman.java
+++ src/main/java/moon/codingmate/_23_refused_bequest/Salesman.java
@@ -1,6 +1,12 @@
 package moon.codingmate._23_refused_bequest;
 
+// Quota 할당량은 Employee, Engineer 보다 Salesman에 특화된 메서드
 public class Salesman extends Employee {
 
+    protected Quota quota;
+
+    protected Quota getQuota() {
+        return new Quota();
+    }
 }
 
